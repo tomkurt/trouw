7fc9df10de2d57ec3c2f6c1ce78267ee9376f8ea
Tom Maeckelberghe <tom@sumocoders.be>
2014-03-03 20:09:13 +0100

    wip

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ $(function(){
     function(){ $('#lichtjes').show() },
     function(){ $('#lichtjes').hide() }
   )
+  $('form').submit(function(e){
+    e.preventDefault();
+    switch($('#code_input').val()){
+      case 'DK3KLJ3':
+        $('*').show()
+        break;
+      case 'LKJD83':
+        $('.dag2deel1').show()
+        break;
+      case 'LKFJD3':
+        $('.dag2deel3').show()
+        break;
+      case 'DLSJDF0':
+        $('.dag2deel2, .dag2deel3').show()
+        break;
+    }
+  })
 })
