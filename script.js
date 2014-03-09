2d55b071b5d2d7e36e5a4c436c2106db79a19245
Tom Maeckelberghe <tom@sumocoders.be>
2014-03-09 12:11:28 +0100

    nav fixed

--- script.js
+++ script.js
@@ -8,20 +8,29 @@ $(function(){
     switch($('#code_input').val()){
       case 'DK3KLJ3':
         $('*').show()
-        $.scrollTo('.dag1', 'slow', {offset: {top: -20, left:0} });
+        $.scrollTo('.dag1', 'slow', {offset: {top: -80, left:0} });
         break;
       case 'LKJD83':
-        $('.dag2deel1').show()
-        $.scrollTo('.dag2deel1', 'slow', {offset: {top: -20, left:0} });
+        $('.dag2deel1, .cadeau').show()
+        $.scrollTo('.dag2deel1', 'slow', {offset: {top: -80, left:0} });
         break;
       case 'LKFJD3':
-        $('.dag2deel3').show()
-        $.scrollTo('.dag2deel3', 'slow', {offset: {top: -20, left:0} });
+        $('.dag2deel3, .cadeau').show()
+        $.scrollTo('.dag2deel3', 'slow', {offset: {top: -80, left:0} });
         break;
       case 'DLSJDF0':
-        $('.dag2deel2, .dag2deel3').show()
-        $.scrollTo('.dag2deel2', 'slow', {offset: {top: -20, left:0} });
+        $('.dag2deel2, .dag2deel3, .cadeau').show()
+        $.scrollTo('.dag2deel2', 'slow', {offset: {top: -80, left:0} });
         break;
+      default:
+        alert('Code niet gevonden, probeer opnieuw');
+    }
+  })
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 950){
+      $('#navwrapper').addClass('display');
+    }else{
+      $('#navwrapper').removeClass('display');
     }
   })
 })
